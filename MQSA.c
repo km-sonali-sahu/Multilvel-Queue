dc7b744647dd41390e1e99734a65d99bb1b3b46b
km sonali sahu <sahusonali5237@gmail.com>
2020-03-28 18:09:16 +0530

    added function Round_Robin_4()
    added  Round_Robin_4() function time quantum 4.

--- MQSA.c
+++ MQSA.c
@@ -15,7 +15,53 @@ int Position,q,Temp1,sum=0,Waiting_Time_2[20],Turn_Around_Time_2[20];
 float Avg_Waiting_Time_2,Avg_Turn_Around_Time_2=0.0;
 
 
-
+//Round_Robin_4() function for highest priority process using time quantum=4.
+void Round_Robin_4()
+{
+	printf("Time Quantum for Queue1 is 4\n");
+	for(i=0;i<j;i++)
+	{
+		Temp[i]=Burst_Time_1[i];
+	} 
+	printf("\nProcess ID\tBurst Time\t Turnaround Time\t Waiting Time\n");
+	m=j;
+    for(i=0,Total=0;m!=0;) 
+    { 
+    	if(Temp[i]<=4&&Temp[i]>0) 
+        {
+			printf("\nProcess[%d] of Queue1 is using CPU for %d units",i+1,Temp[i]); 
+            Total=Total+Temp[i]; 
+            Temp[i]=0; 
+            Count=1; 
+        } 
+        else if(Temp[i]>0) 
+        {
+			printf("\nProcess[%d] of Queue1 is using CPU for 4 units",i+1); 
+            Temp[i]=Temp[i]-4; 
+            Total=Total+4; 
+        } 
+        if(Temp[i]==0&&Count==1) 
+        { 
+            m--; 
+            printf("\nProcess[%d]\t%d\t%d\t%d",i+1,Burst_Time_1[i],Total-Arrival_Time_1[i],Total-Arrival_Time_1[i]-Burst_Time_1[i]);
+            Avg_Waiting_Time_1=Avg_Waiting_Time_1+Total-Arrival_Time_1[i]-Burst_Time_1[i]; 
+            Avg_Turn_Around_Time_1=Avg_Turn_Around_Time_1+Total-Arrival_Time_1[i]; 
+            Count = 0; 
+        } 
+        if(i==j-1) 
+        {
+            i=0; 
+        }
+        else if(Arrival_Time_1[i+1]<=Total) 
+        {
+            i++;
+        }
+        else 
+        {
+            i=0;
+        }
+    } 
+}
 
 int main()
 {
